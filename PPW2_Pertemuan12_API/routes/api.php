19d2a51c3317183902118f8343355b0450827cd3
mazalea <azalea.nareswari@gmail.com>
2023-11-21 23:30:09 +0700

    praktikum

---
+++ PPW2_Pertemuan12_API/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InfoController;
+use App\Http\Controllers\GreetController;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/info', [InfoController::class, 'index'])->name('info');
+Route::get('/greet', [GreetController::class,'greet'])->name('greet');
